f1dd1c47b379096f5260707b5c81316d087caa3c
HenestrosaConH <henestrosaconh@gmail.com>
2022-07-19 12:31:05 +0200

    Código refactorizado y comentarios ordenados

--- pokedex.js
+++ pokedex.js
@@ -1,155 +1,169 @@
-//url de la Api de Pokemon. Con offset indico que quiero que complete esa url del pokemon 0 al 151.
+/**
+ * URL de la API de Pokémon desde el offset hasta el limit.
+ */
+const offset = 0;
+const limit = 151;
+const urlPokemon = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-const urlPokemon = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151`;
-
-//Array con todos los pokemon que nos devuelve la Api.
-//Variable global para recurrir a ella y no constatemente a la Api. Así ganamos rendimiento.
-
+/**
+ * Array con todos los Pokémon que nos devuelve la Api.
+ * Variable global para recurrir a ella y no constantemente a la Api. Así ganamos rendimiento.
+ */
 const allPokemonResults = [];
 
-//Primer fetch para acceder a la API
+document.addEventListener("DOMContentLoaded", function(e) {
+  document.querySelectorAll(".types_selector").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      filterPokemonsByType(event.target.classList[1]);
+    });
+  });
 
+  apiPokemon();
+});
+
+/**
+ * Primer fetch para acceder a la API.
+ */
 const apiPokemon = async () => {
-	const response = await fetch(urlPokemon);
-	const data = await response.json();
-	getPokemon(data.results);
+  const response = await fetch(urlPokemon);
+  const data = await response.json();
+  getPokemon(data.results);
 };
 
-//Segundo fetch para acceder al array de objetos.
-//Incluyo un push para tener un array que sea variable global.
-//Se usará para el buscador y obtener los datos de pokemon de forma más eficiente.
-
+/**
+ * Segundo fetch para acceder al array de objetos. 
+ * Se usará para el buscador y obtener los datos de Pokémon de forma más eficiente.
+ * @param {Array} pokemons Contiene los Pokémon obtenidos de la API.
+ */
 const getPokemon = async (pokemons) => {
-	for (let pokemon of pokemons) {
-		const res = await fetch(pokemon.url);
-		const result = await res.json();
-		allPokemonResults.push(result);
-	}
+  for (let pokemon of pokemons) {
+    const res = await fetch(pokemon.url);
+    const result = await res.json();
+    
+		// Incluyo un push para añadir el Pokémon al array global.
+    allPokemonResults.push(result);
+  }
 
-	printPokemon(allPokemonResults);
+  printPokemon(allPokemonResults);
 };
 
-//Función para crear cada carta pokemon. Se le pasará el array global de pokemon.
-
+/**
+ * Construye las carta Pokémon para adjuntarlas al DOM. 
+ * @param {Array} pokemonToPrint Carga las cargas de todos los Pokémon obtenidos por la API.
+ */
 const printPokemon = (pokemonToPrint) => {
-	//Primero que borre cualquier pokemon anterior que pudiera haber. Mediante innerHTML que borra todo lo que hay antes.
+  // Vacia los hijos del elemento con la clase pokedex para evitar duplicados.
+  const pokedex$$ = document.querySelector(".pokedex");
+  pokedex$$.innerHTML = "";
 
-	const pokedex$$ = document.querySelector(".pokedex");
-	pokedex$$.innerHTML = "";
+  // Para cada Pokémon del array, se crea una carta (div) con id, img, nombre, altura y peso.
+  pokemonToPrint.forEach((pokemon) => {
+    const pokemonCard$$ = document.createElement("div");
+    pokemonCard$$.classList.add("pokemonCard");
 
-	//Para cada pokemon del array crea una carta (div) con: id, img, nombre, altura y peso.
+    const pokemonID$$ = document.createElement("h3");
+    pokemonID$$.classList.add("pokeID");
+    pokemonID$$.textContent = pokemon.id;
 
-	pokemonToPrint.forEach((pokemon) => {
-		const pokemonCard$$ = document.createElement("div");
-		pokemonCard$$.classList.add("pokemonCard");
+    const pokemonPhotoContainer$$ = document.createElement("div");
+    pokemonPhotoContainer$$.classList.add("pokemonPhotoContainer");
 
-		const pokemonID$$ = document.createElement("h3");
-		pokemonID$$.classList.add("pokeID");
-		pokemonID$$.textContent = pokemon.id;
+    const pokemonPhoto$$ = document.createElement("img");
+    pokemonPhoto$$.classList.add("photo");
+    pokemonPhoto$$.setAttribute(
+      "src",
+      pokemon.sprites.other.dream_world.front_default
+    );
 
-		const pokemonPhotoContainer$$ = document.createElement("div");
-		pokemonPhotoContainer$$.classList.add("pokemonPhotoContainer");
+    const pokemonName$$ = document.createElement("h2");
+    pokemonName$$.classList.add("pokeName");
+    pokemonName$$.textContent = pokemon.name;
 
-		const pokemonPhoto$$ = document.createElement("img");
-		pokemonPhoto$$.classList.add("photo");
-		pokemonPhoto$$.setAttribute(
-			"src",
-			pokemon.sprites.other.dream_world.front_default
-		);
+    const pokemonSize$$ = document.createElement("div");
+    pokemonSize$$.classList.add("pokeSize");
 
-		const pokemonName$$ = document.createElement("h2");
-		pokemonName$$.classList.add("pokeName");
-		pokemonName$$.textContent = pokemon.name;
+    const pokemonHeight$$ = document.createElement("p");
+    pokemonHeight$$.classList.add("pokeHeight");
+    pokemonHeight$$.textContent = pokemon.height;
 
-		const pokemonSize$$ = document.createElement("div");
-		pokemonSize$$.classList.add("pokeSize");
+    const pokemonWeight$$ = document.createElement("p");
+    pokemonWeight$$.classList.add("pokeWeight");
+    pokemonWeight$$.textContent = pokemon.weight;
 
-		const pokemonHeight$$ = document.createElement("p");
-		pokemonHeight$$.classList.add("pokeHeight");
-		pokemonHeight$$.textContent = pokemon.height;
+    const pokemonTypeContainer$$ = document.createElement("div");
+    pokemonTypeContainer$$.classList.add("pokeTypeContainer");
 
-		const pokemonWeight$$ = document.createElement("p");
-		pokemonWeight$$.classList.add("pokeWeight");
-		pokemonWeight$$.textContent = pokemon.weight;
+    pokedex$$.appendChild(pokemonCard$$);
+    pokemonCard$$.appendChild(pokemonID$$);
+    pokemonCard$$.appendChild(pokemonPhotoContainer$$);
+    pokemonPhotoContainer$$.appendChild(pokemonPhoto$$);
+    pokemonCard$$.appendChild(pokemonName$$);
+    pokemonCard$$.appendChild(pokemonTypeContainer$$);
 
-		const pokemonTypeContainer$$ = document.createElement("div");
-		pokemonTypeContainer$$.classList.add("pokeTypeContainer");
+    //Invoco a la función que saca los tipos de Pokémon
+    getPokemonType(pokemon.types, pokemonTypeContainer$$);
 
-		pokedex$$.appendChild(pokemonCard$$);
-		pokemonCard$$.appendChild(pokemonID$$);
-		pokemonCard$$.appendChild(pokemonPhotoContainer$$);
-		pokemonPhotoContainer$$.appendChild(pokemonPhoto$$);
-		pokemonCard$$.appendChild(pokemonName$$);
-		pokemonCard$$.appendChild(pokemonTypeContainer$$);
-
-		//Invoco a la función que saca los tipos de pokemon
-		getPokemonType(pokemon.types, pokemonTypeContainer$$);
-
-		pokemonCard$$.appendChild(pokemonSize$$);
-		pokemonSize$$.appendChild(pokemonHeight$$);
-		pokemonSize$$.appendChild(pokemonWeight$$);
-	});
+    pokemonCard$$.appendChild(pokemonSize$$);
+    pokemonSize$$.appendChild(pokemonHeight$$);
+    pokemonSize$$.appendChild(pokemonWeight$$);
+  });
 };
 
-//Función para imprimir uno o dos tipos de pokemon, según tenga.
-
+/**
+ * Obtiene los tipos del Pokémon. Pueden ser uno o dos.
+ * @param {Array} arrayType Contiene los tipos del Pokémon.
+ * @param {HTMLDivElement} pokemonCard Carta a la que se le adjuntan los tipos.
+ */
 const getPokemonType = async (arrayType, pokemonCard) => {
-	if (arrayType.length === 1) {
-		const pokemonType1$$ = document.createElement("p");
-		pokemonType1$$.textContent = arrayType[0].type.name;
-		pokemonCard.appendChild(pokemonType1$$);
-	} else if (arrayType.length > 1) {
-		const pokemonType1$$ = document.createElement("p");
-		pokemonType1$$.textContent = arrayType[0].type.name;
-		pokemonCard.appendChild(pokemonType1$$);
-		const pokemonType2$$ = document.createElement("p");
-		pokemonType2$$.textContent = arrayType[1].type.name;
-		pokemonCard.appendChild(pokemonType2$$);
-	}
+  const pokemonType1$$ = document.createElement("p");
+  pokemonType1$$.textContent = arrayType[0].type.name;
+  pokemonCard.appendChild(pokemonType1$$);
+
+  if (arrayType.length > 1) {
+    const pokemonType2$$ = document.createElement("p");
+    pokemonType2$$.textContent = arrayType[1].type.name;
+    pokemonCard.appendChild(pokemonType2$$);
+  }
 };
 
-//Función para buscar pokemon.
-//El usuario rellena el input y ese resultado lo ponemos en minúsculas y le quitamos posibles espacios delante y detrás.
-//Filtramos el array de todos los pokemon para extraer los que incluyen el texto del input (includes).
-//Los pokemon que cumplen con el input son enviados a la función de imprimir pokemon para generar su carta.
-
+/**
+ * Busca Pokémon por nombre.
+ */
 const search$$ = document
-	.getElementById("search-input")
-	.addEventListener("input", (event) => {
-		const userInput = event.target.value.toLowerCase().trim();
+  .getElementById("search-input")
+  .addEventListener("input", (event) => {
+    // Ponemos el input del usuario en minúsculas y le quitamos posibles espacios delante y detrás.
+    const userInput = event.target.value.toLowerCase().trim();
 
-		const filtered = allPokemonResults.filter((pokemon) => {
-			return pokemon.name.toLowerCase().includes(userInput);
-		});
+    // Filtramos el array de todos los Pokémon para extraer los que incluyen el texto del input (includes).
+    const filtered = allPokemonResults.filter((pokemon) => {
+      return pokemon.name.toLowerCase().includes(userInput);
+    });
 
-		printPokemon(filtered);
-	});
+    // Los Pokémon que cumplen con el input son enviados a la función de imprimir Pokémon para generar su carta.
+    printPokemon(filtered);
+  });
 
-//Función para buscar cada tipo de pokemon a través de unos botones
-
+/**
+ * Busca cada tipo de Pokémon a través de los botones de la selección de tipos.
+ * @param {string} type Tipo por el que filtrar
+ */
 const filterPokemonsByType = (type) => {
-	const filteredPokemonByType = allPokemonResults.filter((pokemon) => {
-		let matchFirstType = false;
-		let matchSecondType = false;
-
-		if (pokemon.types[1]) {
-			matchSecondType = pokemon.types[1].type.name === type;
-		}
+  const filteredPokemonByType = allPokemonResults.filter((pokemon) => {
+    let matchFirstType = false;
+    let matchSecondType = false;
 
 		if (pokemon.types[0]) {
 			matchFirstType = pokemon.types[0].type.name === type;
 		}
 
-		return matchFirstType || matchSecondType;
-	});
-	console.log(filteredPokemonByType);
-	printPokemon(filteredPokemonByType);
+    if (pokemon.types[1]) {
+      matchSecondType = pokemon.types[1].type.name === type;
+    }
+
+    return matchFirstType || matchSecondType;
+  });
+
+  console.log(filteredPokemonByType);
+  printPokemon(filteredPokemonByType);
 };
-
-document.querySelectorAll(".types_selector").forEach((button) => {
-	button.addEventListener("click", (event) => {
-		filterPokemonsByType(event.target.classList[1]);
-	});
-});
-
-apiPokemon();
